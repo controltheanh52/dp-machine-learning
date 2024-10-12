c8c98e23f5c2e7703b39be1059de222f48669d53
controltheanh52 <154208592+controltheanh52@users.noreply.github.com>
2024-10-12 10:25:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('🎈 Machine learning App by Streamlit')
 st.write('Hello, my name is The Anh')
 st.info('this is app builds a ML model!')
 
-with st.expander('data')
+with st.expander('data'):
 st.write('**RAW DATA**')
 read = pd.read_csv('https://raw.githubusercontent.com/controltheanh52/penguin/refs/heads/main/penguins_cleaned.csv')
 read
