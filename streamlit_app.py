7265fcc909b8034fb461e242d0bbcd2548f5d7e3
controltheanh52 <154208592+controltheanh52@users.noreply.github.com>
2024-10-12 10:40:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,14 @@ with st.expander('data'):
 with st.expander('Data visualization'):
   #"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
   st.scatter_chart(data = read, x = "bill_length_mm", y = "body_mass_g", color = 'species')
+
+
+#data preparations
+with st.sidebar:
+  #,"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g",
+  st.header('input the feature')
+
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  gender = st.selectbox('Gender', ('male', 'female'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  
