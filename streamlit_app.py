d069b718a0218a134caf45933682ebe0bf47e4fe
controltheanh52 <154208592+controltheanh52@users.noreply.github.com>
2024-10-12 10:33:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,13 @@ with st.expander('data'):
   read = pd.read_csv('https://raw.githubusercontent.com/controltheanh52/penguin/refs/heads/main/penguins_cleaned.csv')
   read
 
-st.write('**X**')
-X = read.drop('species', axis =  1)
+  st.write('**X**')
+  X = read.drop('species', axis =  1)
 
-st.write('**y**')
-y = read.species
-y
+  st.write('**y**')
+  y = read.species
+  y
+
+with st.expander('Data visualization'):
+  #"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
+  st.scatter_chart(data = read, x = "bill_length_mm", y = "body_mass_g", color = 'species')
